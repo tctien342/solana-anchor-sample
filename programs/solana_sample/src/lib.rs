f75ebdc5efa75117233b30ae49a4608e8bd8460f
Saintno <tctien342@gmail.com>
2022-05-23 12:41:22 +0700

    [Feature] Init todo-list contract

--- programs/solana_sample/src/lib.rs
+++ programs/solana_sample/src/lib.rs
@@ -3,43 +3,131 @@ use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+const MAX_MESSAGE_LEN: usize = 128;
+const MAX_TODO_LEN: usize = 32;
+
 #[program]
 pub mod solana_sample {
-
     use super::*;
 
-    pub fn create(ctx: Context<CreateAccount>) -> ProgramResult {
-        let account = &mut ctx.accounts.account_info;
-        account.level = 0;
+    pub fn create(ctx: Context<CreateAccount>, name: String) -> ProgramResult {
+        let data = &mut ctx.accounts.data;
+        data.name = name;
+        data.notes = [].to_vec();
+        Ok(())
+    }
+    pub fn update_name(ctx: Context<UpdateData>, name: String) -> ProgramResult {
+        let data = &mut ctx.accounts.data;
+        data.name = name;
+        emit!(UpdateNameEvent {
+            name: data.name.clone()
+        });
         Ok(())
     }
 
-    pub fn level_up(ctx: Context<LevelUp>) -> ProgramResult {
-        let account = &mut ctx.accounts.account_info;
-        account.level += 1;
+    pub fn add_note(ctx: Context<UpdateData>, title: String, content: String) -> ProgramResult {
+        let data = &mut ctx.accounts.data;
+        let note = Note {
+            title,
+            content,
+            done: false,
+        };
+        data.notes.push(note);
+        emit!(UpdateNoteEvent {
+            notes: data.notes.clone()
+        });
+        Ok(())
+    }
+
+    pub fn remove_note(ctx: Context<UpdateData>, index: u64) -> ProgramResult {
+        let idx: usize = index.try_into().unwrap();
+        let data = &mut ctx.accounts.data;
+        data.notes.remove(idx);
+        emit!(UpdateNoteEvent {
+            notes: data.notes.clone()
+        });
+        Ok(())
+    }
+
+    pub fn update_note_title(ctx: Context<UpdateData>, index: u64, title: String) -> ProgramResult {
+        let idx: usize = index.try_into().unwrap();
+        let data = &mut ctx.accounts.data;
+        let note = &mut data.notes[idx];
+        note.title = title;
+        emit!(UpdateNoteEvent {
+            notes: data.notes.clone()
+        });
+        Ok(())
+    }
+
+    pub fn update_note_content(
+        ctx: Context<UpdateData>,
+        index: u64,
+        content: String,
+    ) -> ProgramResult {
+        let idx: usize = index.try_into().unwrap();
+        let data = &mut ctx.accounts.data;
+        let note = &mut data.notes[idx];
+        note.content = content;
+        emit!(UpdateNoteEvent {
+            notes: data.notes.clone()
+        });
+        Ok(())
+    }
+
+    pub fn update_note_status(ctx: Context<UpdateData>, index: u64, done: bool) -> ProgramResult {
+        let idx: usize = index.try_into().unwrap();
+        let data = &mut ctx.accounts.data;
+        let note = &mut data.notes[idx];
+        note.done = done;
+        emit!(UpdateNoteEvent {
+            notes: data.notes.clone()
+        });
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct CreateAccount<'info> {
-    #[account(init, payer = user, space = 16 + 8)]
-    pub account_info: Account<'info, UserInfo>,
+    /**
+     * Space for the data
+     * Name: 4 + Maxlen(name) = 4 + 60 = 64
+     * Note: title + content + done = (4 + 60) + (4 + MAX_MESSAGE_LEN) + 1
+     * --> Name + MAX_TODO_LEN * Note
+     */
+    #[account(init, payer = user, space = 16 + 64 + ( 4 + MAX_TODO_LEN * (64 + 4 + MAX_MESSAGE_LEN + 1)))]
+    pub data: Account<'info, User>,
 
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct LevelUp<'info> {
+pub struct UpdateData<'info> {
     #[account(mut)]
-    pub account_info: Account<'info, UserInfo>
+    pub data: Account<'info, User>,
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Note {
+    pub title: String,
+    pub content: String,
+    pub done: bool,
+}
 
 #[account]
-pub struct UserInfo {
-    pub level: u32
+pub struct User {
+    pub name: String,
+    pub notes: Vec<Note>,
 }
 
+#[event]
+pub struct UpdateNoteEvent {
+    pub notes: Vec<Note>,
+}
+
+#[event]
+pub struct UpdateNameEvent {
+    pub name: String,
+}
--- tests/solana_sample.ts
+++ tests/solana_sample.ts
@@ -1,19 +1,31 @@
 import * as assert from 'assert';
 import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+
+import { SolanaSample } from '../target/types/solana_sample';
 
 const { SystemProgram } = anchor.web3;
+
+type TNote = { title: String; content: String; done: boolean };
 
 describe('solana_sample', () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
-  const program = anchor.workspace.SolanaSample;
+  const program = anchor.workspace.SolanaSample as Program<SolanaSample>;
   const baseAccount = anchor.web3.Keypair.generate();
+  const name = 'EzGame';
   anchor.setProvider(provider);
 
+  const handleNoteEvent = (ev) => console.log('UpdateNoteEvent ==> ', ev);
+  const handleNameEvent = (ev) => console.log('UpdateNameEvent ==> ', ev);
+
+  const noteListener = program.addEventListener('UpdateNoteEvent', handleNoteEvent);
+  const nameListener = program.addEventListener('UpdateNameEvent', handleNameEvent);
+
   it('Is created!', async () => {
-    const tx = await program.rpc.create({
+    const tx = await program.rpc.create('EzGame', {
       accounts: {
-        accountInfo: baseAccount.publicKey,
+        data: baseAccount.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       },
@@ -21,18 +33,108 @@ describe('solana_sample', () => {
     });
     console.log('Your transaction signature', tx);
 
-    const account = await program.account.userInfo.fetch(baseAccount.publicKey);
-    console.log('Default Level: ', account.level.toString());
-    assert.ok(account.level.toString() == 0);
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    console.log('Default name: ', account.name.toString());
+    assert.ok(account.name.toString() === name);
   });
 
-  it('Is levelUP!', async () => {
-    await program.rpc.levelUp({
+  it('Update user name', async () => {
+    const newName = 'EzGame Updated!';
+    const tx = await program.rpc.updateName(newName, {
       accounts: {
-        accountInfo: baseAccount.publicKey,
+        data: baseAccount.publicKey,
       },
     });
-    const account = await program.account.userInfo.fetch(baseAccount.publicKey);
-    assert.ok(account.level.toString() == 1);
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    assert.ok(account.name.toString() === newName);
+  });
+
+  it('Add first note!', async () => {
+    const title = 'Working hard';
+    const content = 'Work hard, work smart, play more fuck hard';
+    await program.rpc.addNote(title, content, {
+      accounts: {
+        data: baseAccount.publicKey,
+      },
+    });
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    const notes: TNote[] = account.notes as any;
+    assert.ok(notes.length === 1);
+    assert.ok(notes[0].title === title);
+    assert.ok(notes[0].content === content);
+  });
+
+  it('Update first note title', async () => {
+    const updateIndex = new anchor.BN(0);
+    const newTitle = 'Updated note 1';
+    await program.rpc.updateNoteTitle(updateIndex, newTitle, {
+      accounts: {
+        data: baseAccount.publicKey,
+      },
+    });
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    const notes: TNote[] = account.notes as any;
+    assert.ok(notes.length > 0);
+    assert.ok(notes[0].title === newTitle);
+  });
+
+  it('Update first note content', async () => {
+    const updateIndex = new anchor.BN(0);
+    const newContent = 'Content have been changed';
+    await program.rpc.updateNoteContent(updateIndex, newContent, {
+      accounts: {
+        data: baseAccount.publicKey,
+      },
+    });
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    const notes: TNote[] = account.notes as any;
+    assert.ok(notes.length > 0);
+    assert.ok(notes[0].content === newContent);
+  });
+
+  it('Mark first note as done', async () => {
+    const updateIndex = new anchor.BN(0);
+    await program.rpc.updateNoteStatus(updateIndex, true, {
+      accounts: {
+        data: baseAccount.publicKey,
+      },
+    });
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    const notes: TNote[] = account.notes as any;
+    assert.ok(notes.length > 0);
+    assert.ok(notes[0].done === true);
+  });
+
+  it('Remove first note!', async () => {
+    const updateIndex = new anchor.BN(0);
+    await program.rpc.removeNote(updateIndex, {
+      accounts: {
+        data: baseAccount.publicKey,
+      },
+    });
+    const account = await program.account.user.fetch(baseAccount.publicKey);
+    const notes: TNote[] = account.notes as any;
+    assert.ok(notes.length === 0);
+  });
+
+  // it('Add 32 notes!', async () => {
+  //   for (let i = 0; i < 32; i++) {
+  //     const title = `Note ${i}`;
+  //     const content = `Note ${i}`;
+  //     await program.rpc.addNote(title, content, {
+  //       accounts: {
+  //         data: baseAccount.publicKey,
+  //       },
+  //     });
+  //   }
+  //   const account = await program.account.user.fetch(baseAccount.publicKey);
+  //   const notes: TNote[] = account.notes as any;
+  //   assert.ok(notes.length === 32);
+  // });
+
+  it('Removed all listener', async () => {
+    // Unbind listener
+    program.removeEventListener(noteListener);
+    program.removeEventListener(nameListener);
   });
 });
